60df3887f9abe68e12c86e94a120c4c22430882c
tom2u <tbrodhead@2u.com>
2020-05-21 17:02:31 -0500

    14.4.8

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+//const hbs = exphbs.create({});
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
 
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
